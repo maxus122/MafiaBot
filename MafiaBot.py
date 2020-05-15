4ed92fddcec64a9f20a0bc8cf390cd9f7a2b1e98
maxus122 <r.maximka@mail.ru>
2020-05-16 03:44:31 +0500

    127

--- MafiaBot.py
+++ MafiaBot.py
@@ -5,7 +5,7 @@ from discord.ext.commands import Bot
 from discord.utils import get
 import os
 
-PREFIX = 'Mafia '
+PREFIX = 'Мафия - '
 Bot = commands.Bot(command_prefix = PREFIX)
 Bot.remove_command('help')
 #===================================
